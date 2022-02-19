70fe81f63e36af7477e5a5231e4dbda240e306f7
dev-shahid <shahidul.cse46@gmail.com>
2022-02-19 11:20:22 +0600

    learning node

---
+++ global.js
@@ -0,0 +1,17 @@
+// console.log('Hello World');
+
+// setTimeout(() => {
+//     console.log('I am in setTimeout');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+var a = 10;
+
+function test() {
+    console.log('I am test');
+}
+
+console.log(global.a);
+global.test();
